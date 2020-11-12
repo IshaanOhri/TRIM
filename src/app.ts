99af044f4eea452e1f25c842f8965164f470bb44
Ishaan Ohri <ishaan99ohri@gmail.com>
2020-11-13 03:32:16 +0530

    Added docker files

--- src/app.ts
+++ src/app.ts
@@ -15,52 +15,55 @@ const app = express();
 
 const { PORT, REDIS_PORT } = process.env;
 
-const redisClient: RedisClient = redis.createClient(String(REDIS_PORT));
+const redisClient: RedisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT)
+});
 
 const getAsync = promisify(redisClient.get).bind(redisClient);
 
 redisClient.on('connect', () => {
-	logger.info(`Redis running at http://localhost:${REDIS_PORT}`);
+    logger.info(`Redis running at http://localhost:${REDIS_PORT}`);
 });
 
 redisClient.on('error', () => {
-	logger.info(`Redis connection failure`);
+    logger.info(`Redis connection failure`);
 });
 
 app.use(
-	express.static(path.join(__dirname, '../public'), {
-		index: 'index.html'
-	})
+    express.static(path.join(__dirname, '../public'), {
+        index: 'index.html'
+    })
 );
 
 app.use(
-	morgan((tokens, req, res) => {
-		logger.info(
-			`Method: ${tokens.method(req, res)} URL: ${tokens.url(
-				req,
-				res
-			)} Status: ${tokens.status(req, res)} Resp Time: ${tokens[
-				'response-time'
-			](req, res)} ms`
-		);
-		return null;
-	})
+    morgan((tokens, req, res) => {
+        logger.info(
+            `Method: ${tokens.method(req, res)} URL: ${tokens.url(
+                req,
+                res
+            )} Status: ${tokens.status(req, res)} Resp Time: ${tokens[
+                'response-time'
+            ](req, res)} ms`
+        );
+        return null;
+    })
 );
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/health', async (req, res) => {
-	res.send({
-		success: true,
-		message: message.health
-	});
+    res.send({
+        success: true,
+        message: message.health
+    });
 });
 
 app.use(shortener);
 
 app.listen(PORT, () => {
-	logger.info(`Server running on http://localhost:${PORT}`);
+    logger.info(`Server running on http://localhost:${PORT}`);
 });
 
 export { redisClient, getAsync };
