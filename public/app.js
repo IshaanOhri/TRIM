0e34c5543878ddd8acc9943c35998adb05579ac7
Ishaan Ohri <ishaan99ohri@gmail.com>
2020-11-13 23:55:23 +0530

    Change in docker-compose

--- public/app.js
+++ public/app.js
@@ -105,7 +105,7 @@ async function shortenURL(){
     let json;
 
     if(originalURL != '' && !customURL){
-        const req = await fetch('https://trimz.tk/create', {
+        const req = await fetch('http://localhost:3000/create', {
 			method: 'POST',
 			mode: 'cors',
 			headers: {
@@ -139,7 +139,7 @@ async function shortenURL(){
             }
         }
     }else if(longURL != '' && customURL && shortHandValue != ''){
-        const req = await fetch('https://trimz.tk/create', {
+        const req = await fetch('http://localhost:3000/create', {
 			method: 'POST',
 			mode: 'cors',
 			headers: {
