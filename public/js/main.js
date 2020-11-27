4a34e3c650280e811706e2e6c85e1824ffdf0fb0
IshaanOhri <ishaan99ohri@gmail.com>
2020-11-28 03:30:17 +0530

    js corrected

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,30 @@
 $(document).ready(function () {
-
 	$('#domain-name').hide();
 	$('#short-hand').hide();
 
-    $("#shorten-btn").click(function () {
-		if($('#custom-checkbox').is(':checked')){
-			if(!$('#long-url').val()){
-				alert('Long URL 1');
-			}else if(!$('#short-hand').val()){
-				alert('Short URL 1');
-			}else{
+	$('#shorten-btn').click(function () {
+		if ($('#custom-checkbox').is(':checked')) {
+			if (!$('#long-url').val()) {
+				alert('Long URL not there 1');
+			} else if (!$('#short-hand').val()) {
+				alert('Short hand not there 1');
+			} else {
 				alert('Good 1');
 			}
-		}else{
-			if(!$('#long-url').val()){
-				alert('Long URL 2');
-			}else{
-				alert('Good 2');
-			}
+		} else if (!$('#long-url').val()) {
+			alert('Long URL not there 2');
+		} else {
+			alert('Good 2');
 		}
 	});
 
-	$('#custom-checkbox').change(function() {
-        if(this.checked) {
+	$('#custom-checkbox').change(function () {
+		if (this.checked) {
 			$('#domain-name').show();
 			$('#short-hand').show();
-        }else{
+		} else {
 			$('#domain-name').hide();
 			$('#short-hand').hide();
 		}
-    });
-  });
+	});
+});
